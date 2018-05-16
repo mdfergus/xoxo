0ea058df4cdc4f6dd6c16d114e5b7e368b0a49be
Matt Ferguson <mdfergus@gmail.com>
2018-05-16 12:46:25 -0400

    doesnt run

--- game/index.js
+++ game/index.js
@@ -1,6 +1,25 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const board = Map();
+
+const MOVE = "MOVE";
+export const move = (str, coordArr) => {
+  return {
+    type: MOVE,
+    turn: str,
+    board: coordArr
+  };
+};
+
+export default function reducer(state = { board, turn: "X" }, action) {
+  switch (action.type) {
+    case MOVE:
+      console.log(state, action);
+      return move(
+        state.turn,
+        state.board //.setIn([board[0], board[1]], state.turn)
+      );
+    default:
+      return state;
+  }
 }
